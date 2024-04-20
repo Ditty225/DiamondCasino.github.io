7356e90f62e481941759b49120411827d8f644f3
Ditty225 <126191602+Ditty225@users.noreply.github.com>
2024-04-20 10:44:52 -0500

    Update script.js

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ function submitOrder() {
     document.getElementById('total').textContent = '$' + total.toFixed(2);
 
     // Send order to Google Sheet via Google Apps Script Web App
-    fetch('https://script.google.com/macros/s/AKfycbwA9jVU61JR1ztoSouJOtp3xy_EYY4d_qjcDbxqvOcm_6Lu-dsDQy-zuqCS22Lp_II_HQ/exec', {  // Replace 'YOUR_WEB_APP_URL' with your actual endpoint URL
+    fetch('https://script.google.com/macros/s/AKfycbzPCuUlKElyIjrpalvYlikhAgHFVn0Lxjwru6HUAp9MFmN0maehLBiGd_ajW6oWbok-bw/exec', {  // Replace 'YOUR_WEB_APP_URL' with your actual endpoint URL
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
